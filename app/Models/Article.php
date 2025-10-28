5e2db14d1a7838f775a6a2eed68b1418f134e0db
Batiste0912 <batiste.dubo@outlook.fr>
2025-10-28 11:08:52 +0100

    :sparkles: Add categories relationship to Article model and migration

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Article extends Model
 {
@@ -24,5 +25,10 @@ class Article extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function categories()
+    {
+        return $this->hasMany(Category::class);
     }
 }
